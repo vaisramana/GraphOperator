3a17f8f2edb6fbc28efa59448f719a337336cded
shuo <zhangshuo.chn@gmail.com>
2019-09-09 17:51:22 +0800

    zzz

--- model.py
+++ model.py
@@ -7,6 +7,8 @@ import sys
 import os
 import tensorflow as tf
 from google.protobuf import text_format
+import numpy as np
+import imageio
  
 
 
@@ -60,11 +62,39 @@ class model:
         summaryWriter = tf.compat.v1.summary.FileWriter('log/', graph)
 
 
-    def get_tensor_by_name(self, tensor_name):
-        tensor = tf.import_graph_def(self._graph_def, name='', return_elements=[tensor_name])[0]
-        return tensor
- 
+    def inference(self, input_tensor_name_list, input_data_list, trace_tensor_name_list):
+        tf.import_graph_def(self._graph_def, name='')
+        with tf.Session() as sess:
+            init = tf.global_variables_initializer()
+            sess.run(init)
+            
+            trace_tensor_list = []
+            for tensor_name in trace_tensor_name_list:
+                trace_tensor_list.append(sess.graph.get_tensor_by_name(tensor_name))
 
+            input_tensor_list = []
+            for tensor_name in input_tensor_name_list:
+                input_tensor_list.append(sess.graph.get_tensor_by_name(tensor_name))
+            
+            feed_dict = {}
+            for input_tensor,input_data in zip(input_tensor_list, input_data_list):
+                feed_dict[input_tensor] = input_data
+            
+            outputs = sess.run(trace_tensor_list,
+                              feed_dict=feed_dict)
+
+            if len(outputs) != len(trace_tensor_list):
+                print("inference error")
+                assert(0)
+
+            return outputs        
+
+
+
+    def quantize(self):
+        target_tensor_name = "CifarNet/conv1/weights/read:0"
+        
+        
 
 
 
@@ -76,12 +106,26 @@ if __name__ == '__main__':
     #m.write_summary()
 
     # insert add after postprobs
-    const_value = tf.constant([1.], name="fake_const")
-    target_tensor = m.get_tensor_by_name("postprobs:0")
-    tf.add(target_tensor, const_value, name="FakeAdd")
-    m._graph_def = tf.compat.v1.get_default_graph().as_graph_def()
-    m.write_pbtxt()
+    #const_value = tf.constant([1.], name="fake_const")
+    #target_tensor = m.get_tensor_by_name("postprobs:0")
+    #tf.add(target_tensor, const_value, name="FakeAdd")
+    #m._graph_def = tf.compat.v1.get_default_graph().as_graph_def()
+    #m.write_pbtxt()
      
+
+    # test cifar
+    trace_tensor_list = ["CifarNet/Predictions/Reshape_1:0"]
+    input_tensor_list = ["Placeholder:0"]
+    #im = imageio.imread("../../../data/tmp/images/test_49_6.png")
+    im = imageio.imread(sys.argv[2])
+    im = np.expand_dims(np.array(im), axis=0)
+    input_data_list = [im]
+    outputs = m.inference(input_tensor_list, input_data_list, trace_tensor_list)
+    for tensor,data in zip(trace_tensor_list,outputs):
+        print("%s\n%r" %(tensor, data))
+
+
+
     
     
 
