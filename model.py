3437265cc68bac467819f9ee768ff0db064cfcc2
shuo <zhangshuo.chn@gmail.com>
2019-09-10 14:20:02 +0800

    zzz

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ import sys
 import os
 import tensorflow as tf
 from tensorflow.contrib.quantize.python import quant_ops
+from tensorflow.core.framework import tensor_shape_pb2
 from google.protobuf import text_format
 import numpy as np
 import imageio
@@ -35,6 +36,8 @@ class model:
         self._filepath = filepath
         self._folder,tempfilename = os.path.split(filepath)
         self._filename,self._extension = os.path.splitext(tempfilename)
+        self._graph_def = None
+        self._quant_graph_def = None
        
         if self._extension == ".pb":
             self._graph_def = self.read_pb()
@@ -46,9 +49,12 @@ class model:
         assert(self._graph_def != None)
 
 
-    def import_graph_def(self):
+    def import_graph_def(self, is_quant=False):
         tf.reset_default_graph()
-        tf.import_graph_def(self._graph_def, name='')
+        graph_def = self._quant_graph_def if is_quant else self._graph_def
+        if graph_def == None:
+            raise Exception("graph def is null")
+        tf.import_graph_def(graph_def, name='')
 
 
     def write_pbtxt(self, is_quant=False):
@@ -130,6 +136,50 @@ class model:
        return node
 
 
+    def inference_quant(self, input_tensor_name_list, input_data_list, trace_tensor_name_list):
+        #tf.import_graph_def(self._graph_def, name='')
+        with tf.Session() as sess:
+            init = tf.global_variables_initializer()
+            sess.run(init)
+            
+            trace_tensor_list = []
+            for tensor_name in trace_tensor_name_list:
+                trace_tensor_list.append(sess.graph.get_tensor_by_name(tensor_name))
+
+            input_tensor_list = []
+            for tensor_name in input_tensor_name_list:
+                input_tensor_list.append(sess.graph.get_tensor_by_name(tensor_name))
+            
+            feed_dict = {}
+            for input_tensor,input_data in zip(input_tensor_list, input_data_list):
+                feed_dict[input_tensor] = input_data
+            
+            quant_bits = 8
+            is_training = True
+            target_weight_tensor_name = "CifarNet/conv1/weights:0"
+            target_weight_tensor = sess.graph.get_tensor_by_name(target_weight_tensor_name)
+            target_weight_tensor_quant = quant_ops.LastValueQuantize(
+                        target_weight_tensor,
+                        is_training=is_training,
+                        narrow_range=True,
+                        num_bits=quant_bits,
+                        name_prefix="Weights")
+            
+            trace_tensor_list.append(target_weight_tensor)
+            trace_tensor_list.append(target_weight_tensor_quant)
+
+            outputs = sess.run(trace_tensor_list,
+                              feed_dict=feed_dict)
+
+            if len(outputs) != len(trace_tensor_list):
+                print("inference error")
+                assert(0)
+
+            for tensor,data in zip(trace_tensor_list,outputs):
+                print("%s\n%r" %(tensor, data))
+            return outputs        
+
+        
 
     def quantize(self):
         self._quant_graph_def = tf.GraphDef()
@@ -149,12 +199,56 @@ class model:
         print(conv2d_node_dict)
 
 
+    def convert_glass(self):
+        new_graph_def = tf.GraphDef()
+        with tf.Session() as sess:
+            for node in sess.graph_def.node:
+                if node.name == "conv_1_conv2d_1_2/Pad":
+                    # add placeholder
+                    placeholder_node = new_graph_def.node.add()
+                    placeholder_node.op = "Placeholder" 
+                    placeholder_node.name = "image_in"
+                    placeholder_node.attr['dtype'].type = 1
+                    s = tf.TensorShape([1, 112, 112, 1])
+                    placeholder_node.attr['shape'].shape.CopyFrom(s.as_proto())
+                    
+                    # modified input
+                    pad_node = new_graph_def.node.add()
+                    pad_node.op = node.op 
+                    pad_node.name = node.name 
+                    pad_node.device = node.device
+                    pad_node.attr['T'].type = node.attr['T'].type
+                    pad_node.attr['Tpaddings'].type = node.attr['Tpaddings'].type
+
+                    assert(len(node.input)==2)
+                    is_target_input_found = False
+                    keep_input_tensor = None
+                    for i in node.input:
+                        if i == "fifo_queue_DequeueMany":
+                            is_target_input_found = True
+                        else:
+                            keep_input_tensor = i
+                    if not is_target_input_found:
+                        assert(0)
+
+                    pad_node.input.extend([placeholder_node.name])
+                    pad_node.input.extend([keep_input_tensor])
+                    
+                else:
+                    new_node = new_graph_def.node.add()
+                    new_node.CopyFrom(node)
+        print("write pb to %s_ph.pb" %self._filename)
+        tf.io.write_graph(new_graph_def, './', self._filename+"_ph.pb", as_text=False)
+        tf.io.write_graph(new_graph_def, './', self._filename+"_ph.pbtxt", as_text=True)
+
+
 
 if __name__ == '__main__':
     input_model = sys.argv[1]
     #convert_pb_to_pbtxt(input_model)
     m = model(input_model)
     m.import_graph_def()
+    #m.convert_glass()
     #m.write_pbtxt()
     #m.write_summary()
 
@@ -166,19 +260,28 @@ if __name__ == '__main__':
      
 
     # test cifar
-    trace_tensor_list = ["CifarNet/Predictions/Reshape_1:0"]
-    input_tensor_list = ["Placeholder:0"]
-    #im = imageio.imread("../../../data/tmp/images/test_49_6.png")
-    im = imageio.imread(sys.argv[2])
-    im = np.expand_dims(np.array(im), axis=0)
+    #trace_tensor_list = ["CifarNet/Predictions/Reshape_1:0"]
+    #input_tensor_list = ["Placeholder:0"]
+    ##im = imageio.imread("../../../data/tmp/images/test_49_6.png")
+    #im = imageio.imread(sys.argv[2])
+    #im = np.expand_dims(np.array(im), axis=0)
+    #input_data_list = [im]
+    ##outputs = m.inference(input_tensor_list, input_data_list, trace_tensor_list)
+    #outputs = m.inference_quant(input_tensor_list, input_data_list, trace_tensor_list)
+
+
+
+    # test glass 
+    trace_tensor_list = ["pre_fc1_285_2/Reshape_1:0"]
+    input_tensor_list = ["image_in:0"]
+    im = np.ones([112,112])
     input_data_list = [im]
     outputs = m.inference(input_tensor_list, input_data_list, trace_tensor_list)
-    for tensor,data in zip(trace_tensor_list,outputs):
-        print("%s\n%r" %(tensor, data))
 
 
-    m.quantize()
-    m.write_pbtxt(is_quant=True)
+
+    #m.quantize()
+    #m.write_pbtxt(is_quant=True)
 
     
     
